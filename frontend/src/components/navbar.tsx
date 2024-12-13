612679a0fee469667f5c443b3d04220ce2fd2b69
raefknani <raef.knani@gmail.com>
2024-12-13 18:19:41 +0100

    nav barr desktop

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,16 +1,15 @@
 import TaskIMG from "../assets/icons/taskIMG.png";
+import "../Css/ResponsiveNavBar.css";
 function Navbar() {
   return (
-    <div className="bg-[#EAEAEA] h-[86px] flex items-center">
-      <ul className="flex items-center space-x-4 ml-[23px]">
-        <li>
-          <i className="bi bi-list text-[40px]"></i>
+    <div className="bg-[#EAEAEA] h-[65px] flex items-center">
+      <ul className="flex items-center space-x-4 ml-[23px] w-full justify-between">
+        <li className="flex items-center space-x-2">
+          <img src={TaskIMG} alt="none" className="h-[35px] mr-[20px]" />
+          <p className="text-[20px]">TaskMate</p>
         </li>
-        <li>
-          <p className="ml-[25px] text-[25px]">TaskMate</p>
-        </li>
-        <li>
-          <img src={TaskIMG} alt="none" className="h-[40px]" />
+        <li className="ml-auto">
+          <div className="h-[45px] w-[45px] rounded-[30px] bg-[#CFCFCF] UserPic mr-[23px]"></div>
         </li>
       </ul>
     </div>
