b7d66125e90043af1513bf105e8b36e2d57d7603
raefknani <raef.knani@gmail.com>
2024-12-13 18:42:05 +0100

    chnage navbar

--- frontend/src/components/navbar2.tsx
+++ frontend/src/components/navbar2.tsx
@@ -1,12 +1,13 @@
 import TaskIMG from "../assets/icons/taskIMG.png";
 import "../Css/ResponsiveNavBar.css";
-function Navbar() {
+function Navbar2() {
   return (
     <div className="bg-[#EAEAEA] h-[65px] flex items-center">
       <ul className="flex items-center space-x-4 ml-[23px] w-full justify-between">
         <li className="flex items-center space-x-2">
-          <img src={TaskIMG} alt="none" className="h-[35px] mr-[20px]" />
-          <p className="text-[20px]">TaskMate</p>
+          <i className="bi bi-list text-[30px] burgerMenue"></i>
+          <img src={TaskIMG} alt="none" className="h-[35px] mr-[20px] hidden" />
+          <p className="text-[20px] hidden">TaskMate</p>
         </li>
         <li className="ml-auto">
           <div className="h-[45px] w-[45px] rounded-[30px] bg-[#CFCFCF] UserPic mr-[10px]"></div>
@@ -16,4 +17,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default Navbar2;
